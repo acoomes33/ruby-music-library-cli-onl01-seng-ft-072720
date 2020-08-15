b10f96bbe8122d7b9286025db3a713d567ea05d1
Alan Coomes <acoomes1@cord.edu>
2020-08-15 18:07:00 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -25,6 +25,7 @@ class MusicLibraryController
     sorted_songs = Song.all.sort {|a,b| a.name <=> b.name}
     sorted songs.each_with_index do |song, i|
       puts "#{i+1}. #{song}"
+    end 
   end 
   
   def list_artists
