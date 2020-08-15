e6787533912ec258bf17f36a92ac428888b1b22a
Alan Coomes <acoomes1@cord.edu>
2020-08-15 18:09:52 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -22,8 +22,7 @@ class MusicLibraryController
   #CLI methods 
   
   def list_songs
-    sorted_songs = Song.all.sort {|a,b| a.name <=> b.name}
-    sorted_songs.each_with_index do |s, i|
+    Song.all.sort{|a,b| a.name <=> b.name}.each_with_index do |s, i|
       puts "#{i+1}. #{s.artist} - #{s.name} - #{s.genre}"
     end 
   end 
